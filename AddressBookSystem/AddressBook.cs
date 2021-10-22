c26174c80908e9247cc7c8a9a8e20a2265f19491
swapnil-bagul <bagulswapnilp@gmail.com>
2021-10-22 14:45:59 +0530

    [Swapnil] Added UC1 Ability to Create Contact

--- AddressBookSystem/AddressBook.cs
+++ AddressBookSystem/AddressBook.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace AddressBookSystem
 {
@@ -7,33 +8,30 @@ namespace AddressBookSystem
         private string firstName, lastName, address, city, state, email;
         private int zip;
         private long phoneNumber;
-        public ContactDetails (string firstName,string lastName,string address,string city,string state,int zip,long phoneNumber,string email)
+
+        public void setDetails()
         {
-            this.firstName = firstName;
-            this.lastName = lastName;
-            this.address = address;
-            this.city = city;
-            this.state = state;
-            this.zip = zip;
-            this.phoneNumber = phoneNumber;
-            this.email = email;
-            
+            Console.WriteLine("Enter Your Contact Deatils: ");
+            Console.Write("Enter First Name: ");
+            firstName = Console.ReadLine();
+            Console.Write("Enter Last Name: ");
+            lastName = Console.ReadLine();
+            Console.Write("Enter Address: ");
+            address = Console.ReadLine();
+            Console.Write("Enter City: ");
+            city = Console.ReadLine();
+            Console.Write("Enter State: ");
+            state = Console.ReadLine();
+            Console.Write("Enter Zip: ");
+            zip = Convert.ToInt32 (Console.ReadLine());
+            Console.Write("Enter Phone Number: ");
+            phoneNumber = Convert.ToInt64 (Console.ReadLine());
+            Console.Write("Enter Email: ");
+            email = Console.ReadLine();
         }
         public void getDetails()
         {
-            Console.WriteLine("Contact Details:\n"+"First Name: "+firstName+"\nLast Name: "+lastName+"\nAddress: "+address+"\nCity: "+city +"\nState: "+state +"\nZip: "+zip+"Phone Number: "+phoneNumber +"\nEmail: "+email    );
-        }
-    }
-
-    class AddressBook
-    {
-
-        static void Main(string[] args)
-        {
-            ContactDetails details = new ContactDetails("Swapnil","Bagul","Nandurbar","Nandurbar","Maharastra",425412,7410177019,"swapnilbagul@gmail.com");
-            details.getDetails();
-            Console.ReadLine();
-       
+            Console.WriteLine("Contact Details:\n" + "First Name: " + firstName + "\nLast Name: " + lastName + "\nAddress: " + address + "\nCity: " + city + "\nState: " + state + "\nZip: " + zip + "Phone Number: " + phoneNumber + "\nEmail: " + email);
         }
     }
 }

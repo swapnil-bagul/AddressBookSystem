c8f298292d597a0e886d2eca9d4de9d8943d28da
swapnil-bagul <bagulswapnilp@gmail.com>
2021-10-22 19:14:50 +0530

    [Swapnil] Added UC3 Ability to Edit Existing Contact

--- AddressBookSystem/AddressBookMain.cs
+++ AddressBookSystem/AddressBookMain.cs
@@ -8,23 +8,49 @@ namespace AddressBookSystem
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("----------------------Welcome to Address Book----------------------");
-            Console.WriteLine("1. Add Contact\n");
-            Console.WriteLine("Enter a Option");
-            int option = Convert.ToInt32(Console.ReadLine());
-            
-            switch (option )
+            ContactDetails addressBook = new ContactDetails();
+            string yes = "y";
+            string y;
+
+            do
             {
-                case 1:
-                    ContactDetails contact = new ContactDetails();
-                    contact.setDetails();
-                    Console.WriteLine("---------------------------------------------------------------");
-                    contact.getDetails();
-                    break;
+                Console.WriteLine("----------------------Welcome to Address Book----------------------");
+                Console.WriteLine("1. Add Contact\n2. Edit Contact");
+                Console.WriteLine("Enter a Option");
+                int option = Convert.ToInt32(Console.ReadLine());
+
+                switch (option)
+                {
+                    case 1:
+                        Console.Write("Enter number of contact you are Adding: ");
+                        int num = Convert.ToInt32(Console.ReadLine());
+                        for (int i = 1; i <= num; i++)
+                        {
+                            
+                            addressBook.setDetails();
+                            Console.WriteLine("---------------------------------------------------------------");
+                            addressBook.getDetails();
+
+                        }
+                        break;
+                    case 2:
+                        addressBook.editContact();
+                        break;
+
+
+
+                }
+                Console.WriteLine("Do you Want to Continue press [ y/n ] ");
+                y = Console.ReadLine();
 
             }
-            
+            while (yes == y);
+
+
+
         }
+       
+        
     }
    
 }

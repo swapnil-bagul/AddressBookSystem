4bef65ebce8b5cd09c001ba57367c837631ad9d8
swapnil-bagul <bagulswapnilp@gmail.com>
2021-10-22 14:17:50 +0530

    practice

--- AddressBookSystem/AddressBook.cs
+++ AddressBookSystem/AddressBook.cs
@@ -3,65 +3,34 @@ using System.Collections.Generic;
 
 namespace AddressBookSystem
 {
-    class AddContactDetails
+    class ContactDetails
     {
-        //List Data Structure
-        
-        public static void SetDetails()
+        private string firstName, lastName, address, city, state, email;
+        private int zip;
+        private long phoneNumber;
+        public void setDetails()
         {
-            List<String> Details = new List<string>();
-            List<long> Details1 = new List<long>();
-            Console.Write("Enter The Number of Person Details You Are Adding:");
-            int numOfPerson = Convert.ToInt32(Console.ReadLine());
-            int startNum = 1;
-
-            while (startNum <=numOfPerson )
-            {
-
-                Console.WriteLine("Enter Your Contact Deatils: ");
-                Console.Write("Enter First Name: ");
-                string firstName = Console.ReadLine();
-                Details.Add(firstName);
-                Console.Write("Enter Last Name: ");
-                string lastName = Console.ReadLine();
-                Details.Add(lastName);
-                Console.Write("Enter Address: ");
-                string address = Console.ReadLine();
-                Details.Add(address );
-                Console.Write("Enter City: ");
-                string city = Console.ReadLine();
-                Details.Add(city);
-                Console.Write("Enter State: ");
-                string state = Console.ReadLine();
-                Details.Add(state);
-                Console.Write("Enter Phone Number: ");
-                long phoneNumber =Convert.ToInt64(Console.ReadLine());
-                Details1.Add(phoneNumber);
-                Console.Write("Enter Email: ");
-                string email = Console.ReadLine();
-                Details.Add(email);
-                Console.Write("Enter Zip: ");
-                long zip = Convert.ToInt32 (Console.ReadLine());
-                Details1.Add(zip);
-
-                startNum++;
-            }
-            Console.WriteLine("Number Of Contact In Address Book : " + startNum);
-            Details.ForEach(Console.WriteLine);
-            Console.WriteLine("Contact Information: ");
-            Details1.ForEach(Console.WriteLine);
-
-
+            Console.WriteLine("Enter Your Contact Deatils: ");
+            Console.Write("Enter First Name: ");
+            firstName = Console.ReadLine();
+            Console.Write("Enter Last Name: ");
+            lastName = Console.ReadLine();
+            Console.Write("Enter Address: ");
+            address = Console.ReadLine();
+            Console.Write("Enter City: ");
+            city = Console.ReadLine();
+            Console.Write("Enter State: ");
+            state = Console.ReadLine();
+            Console.Write("Enter Zip: ");
+            zip = Convert.ToInt32 (Console.ReadLine());
+            Console.Write("Enter Phone Number: ");
+            phoneNumber = Convert.ToInt64 (Console.ReadLine());
+            Console.Write("Enter Email: ");
+            email = Console.ReadLine();
         }
-    }
-
-    class AddressBook
-    {
-        static void Main(string[] args)
+        public void getDetails()
         {
-            AddContactDetails.SetDetails();
-          
-           
+            Console.WriteLine("Contact Details:\n" + "First Name: " + firstName + "\nLast Name: " + lastName + "\nAddress: " + address + "\nCity: " + city + "\nState: " + state + "\nZip: " + zip + "Phone Number: " + phoneNumber + "\nEmail: " + email);
         }
     }
 }
---
+++ AddressBookSystem/AddressBookMain.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AddressBookSystem
+{
+    class AddressBookMain
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("----------------------Welcome to Address Book----------------------");
+            ContactDetails contact = new ContactDetails();
+            contact.setDetails();
+            contact.getDetails();
+        }
+    }
+   
+}
